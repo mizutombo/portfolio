722e8a1f3d9ba02d4c5600309c1f96676e6fb011
mizutombo <thomas.shultz52@gmail.com>
2016-09-20 23:02:41 -0700

    added scripts ... jquery, projects, blogProjects ... revd index file

---
+++ scripts/blogProjects.js
@@ -0,0 +1,42 @@
+var ourLocalData = [
+  {
+    title:       'Portfolio',
+    category:    'jQuery, Javascript, HTML, CSS',
+    developer:   'Tom Shultz',
+    developerUrl:   'https://github.com/mizutombo/portfolio/tree/class-02',
+    publishedOn: '2016-09-20',
+    body:        '<p>description</p>'
+  },
+  {
+    title:       '4-the-Rodentz',
+    category:    'Javascript, HTML, CSS',
+    developer:   'Tom Shultz, Elizabeth Fillmore, Drew Stock, Jon Walz, Lee Zamastil',
+    developerUrl:   'https://github.com/4-the-rodentz/App',
+    publishedOn: '2016-08-18',
+    body:        '<p>description</p>'
+  },
+  {
+    title:       'Market Analysis',
+    category:    'Javascript, HTML, CSS',
+    developer:   'Tom Shultz',
+    developerUrl:   'https://github.com/mizutombo/market-analysis-app/tree/class13',
+    publishedOn: '2016-08-09',
+    body:        '<p>description</p>'
+  },
+  {
+    title:       'Cookie Stand',
+    category:    'Javascript, HTML, CSS',
+    developer:   'Tom Shultz',
+    developerUrl:   'https://github.com/mizutombo/cookie-stand/tree/class09',
+    publishedOn: '2016-07-26',
+    body:        '<p>description</p>'
+  },
+  {
+    title:       'Guessing Game',
+    category:    'Javascript, HTML, CSS',
+    developer:   'Tom Shultz',
+    developerUrl:   'https://github.com/mizutombo/guessing-game/tree/gh-pages',
+    publishedOn: '2016-07-11',
+    body:        '<p>description</p>'
+  },
+];
