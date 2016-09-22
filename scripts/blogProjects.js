3677a82749b95793f00f1dc2e65b1d2ee445fbae
mizutombo <thomas.shultz52@gmail.com>
2016-09-21 23:30:29 -0700

    added code and script  to create filters and activate tabs

--- scripts/blogProjects.js
+++ scripts/blogProjects.js
@@ -1,23 +1,23 @@
 var ourLocalData = [
   {
     title:       'Portfolio',
-    category:    'jQuery, Javascript, HTML, CSS',
+    category:    'Code 301 personal project',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/portfolio/tree/class-02',
     publishedOn: '2016-09-20',
-    body:        '<p>description</p>'
+    body:        '<p>Software Development Portfolio Blog</p>'
   },
   {
     title:       '4-the-Rodentz',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 team project',
     developer:   'Tom Shultz, Elizabeth Fillmore, Drew Stock, Jon Walz, Lee Zamastil',
     developerUrl:   'https://github.com/4-the-rodentz/App',
     publishedOn: '2016-08-18',
-    body:        '<p>description</p>'
+    body:        '<p>Team final project for Code 201</p>'
   },
   {
     title:       'Market Analysis',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 individual assignment',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/market-analysis-app/tree/class13',
     publishedOn: '2016-08-09',
@@ -25,7 +25,7 @@ var ourLocalData = [
   },
   {
     title:       'Cookie Stand',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 individual assignment',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/cookie-stand/tree/class09',
     publishedOn: '2016-07-26',
@@ -33,7 +33,7 @@ var ourLocalData = [
   },
   {
     title:       'Guessing Game',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 individual assignment',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/guessing-game/tree/gh-pages',
     publishedOn: '2016-07-11',
