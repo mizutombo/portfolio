50b70d3bce3c867402f791b319a457e750672da9
mizutombo <thomas.shultz52@gmail.com>
2016-09-25 19:32:57 -0700

    refactored code in scripts and index ... debugged to enable full functionality

--- scripts/projects.js
+++ scripts/projects.js
@@ -9,41 +9,41 @@ function Project (opts) {
   this.body = opts.body;
 };
 
+//Project.prototype.toHtml = function() {
+/* incorporate handlebars.js into code
+  var source = $('#article-template').html();
+  var template = Handlebars.compile(source);
+  var html = template(this);
+
+  return html;
+}; */
+
 Project.prototype.toHtml = function() {
+var $newProject = $('article.template').clone();
+$newProject.removeAttr('class');
 
-  /*Project.prototype.toHtml = function() {*/
-   var source = $('#article-template').html();
-   var template = Handlebars.compile(source);
-   var html = template(this);
+$newProject.attr('data-category', this.category);
+$newProject.find('a:first').text(this.developer);
+$newProject.find('a').attr('href', this.developerUrl);
+$newProject.find('h2').text(this.title); // return project title into <h2></h2> spot
+$newProject.find('h3').text(this.category); // return project category into <h3></h3> spot
+$newProject.find('time[pubdate]').attr('title', this.publishedOn);
+$newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+$newProject.find('h3.project-body').html(this.body);
+//$newProject.find('h3.project-body').after(this.body); // return project description into <h4></h4> spot
 
-   return html;
-  };
+return $newProject;
+};
 
-
-  /*var $newProject = $('#project.template').clone();
-  $newProject.attr('data-category', this.category);
-  $newProject.find('a').text(this.developer);
-  $newProject.find('a').attr('href', this.developerUrl);
-  $newProject.find('h1').text(this.title);
-  $newProject.find('div.byLine').after(this.body);
-  $newProject.find('time[pubdate]').attr('title', this.publishedOn);
-  $newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-
-  $newProject.removeAttr('class');*/
-
-  //return $newProject;//
-//};//
-
-myLocalData.sort(function(curElem, nextElem) {
-  return (new Date(nextElem.publishedOn)) - (new Date(curElem.publishedOn));
+myLocalData.sort(function(currentElement, nextElement) {
+  return (new Date(nextElement.publishedOn)) - (new Date(currentElement.publishedOn));
 });
 
-myLocalData.forEach(function(ele) {
-  projects.push(new Project(ele));
+myLocalData.forEach(function(element) {
+  projects.push(new Project(element));
 });
 
-projects.forEach(function(a) {
-  $('#project').append(a.toHtml());
-  $('#developer-filter').append('<option value ="' + a.developer + '">' + a.developer + '</option>');
-  $('#category-filter').append('<option value ="' + a.category + '">' + a.category + '</option>');
+projects.forEach(function(project) {
+  $('#project').append(project.toHtml());
+  //populate contents of projects via append onto section with id='project'
 });
--- scripts/projectsView.js
+++ scripts/projectsView.js
@@ -1,18 +1,20 @@
 var projectView = {};
 
 projectView.populateFilters = function() {
-  $('article').not('.template').each(function() {
-    var developerName, category, optionTag;
+  $('article').not('.template').each(function() { // "not('.template')" enables new projects to be added into 'article', together with associated population of pull-down selector names for new Developer and new Project Category
+    var developer;
+    var category;
+    var optionTag;
 
-    developerName = $(this).find('address a').text();
-    optionTag = '<option value = "' + developerName + '">' + developerName + '</option>';
-    $('#developer-filter').append(optionTag);
-
-    category = $(this).attr('data-category');
+    developer = $(this).find('address a').text(); // retrieves text
+    optionTag = '<option value = "' + developer + '">' + developer + '</option>';
+    if ($('#developer-filter option[value = "' + developer + '"]').length === 0) {
+      $('#developer-filter').append(optionTag); // populates Developer name for pull-down selector and appends via option tag, without duplicating Developer name in selector
+    }
+    category = $(this).attr('data-category'); // retrieves attribute and value
     optionTag = '<option value = "' + category + '">' + category + '</option>';
-
     if ($('#category-filter option[value = "' + category + '"]').length === 0) {
-      $('#category-filter').append(optionTag);
+      $('#category-filter').append(optionTag); // populates Project Category name for pull-down selector and appends via option tag, without duplicating Project Category name in selector
     }
   });
 };
@@ -20,16 +22,15 @@ projectView.populateFilters = function() {
 projectView.handleDeveloperFilter = function() {
   $('#developer-filter').on('change', function() {
     if ($(this).val()) {
-      $('#project').hide();
+      $('article').hide(); // upon filter item change-click, hide all projects in 'article'
       var $same = $(this).val();
-      $('#project').each(function() {
-        if ($same === $(this).find('address a').text()) {
-          $(this).fadeIn();
+      $('article').each(function() {
+        if ($same === $(this).find('address a').text()) { // retrieves text
+          $(this).fadeIn(); // fade into 'article' view the click-selected item
         }
       });
-
     } else {
-      $('#project').not('.template').show();
+      $('article').not('.template').show(); // if no filter item is selected, show all projects
     }
     $('#category-filter').val('');
   });
@@ -38,15 +39,15 @@ projectView.handleDeveloperFilter = function() {
 projectView.handleCategoryFilter = function() {
   $('#category-filter').on('change', function() {
     if ($(this).val()) {
-      $('#project').hide();
+      $('article').hide(); // upon filter item change-click, hide all projects in 'article'
       var $same = $(this).val();
-      $('#project').each(function() {
-        if ($same === $(this).attr('data-category')) {
-          $(this).fadeIn();
+      $('article').each(function() {
+        if ($same === $(this).attr('data-category')) { // retrieves attribute and value
+          $(this).fadeIn(); // fade into 'article' view the click-selected item
         }
       });
     } else {
-      $('#project').not('.template').show();
+      $('article').not('.template').show(); // if no filter item is selected, show all projects
     }
     $('#developer-filter').val('');
   });
@@ -55,12 +56,11 @@ projectView.handleCategoryFilter = function() {
 projectView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function(event) {
     event.preventDefault();
-    $('.tab-content').hide();
+    $('.tab-content').hide(); // upon click of any navigation link, hide all '.tab-content' items ... i.e. hide all projects and contents of "About" page
     var $selectedItem = $(this).data('content');
-    $('#' + $selectedItem).fadeIn();
+    $('#' + $selectedItem).fadeIn(); // fade in page linked to click-selected navigation link
   });
-
-  $('.main-nav .tab:first').click(); //calls jQuery event
+  $('.main-nav .tab:first').click(); //calls jQuery event on navigation
 };
 
 projectView.setTeasers = function() {
