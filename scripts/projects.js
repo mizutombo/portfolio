e6497d58732883ff3ecd13188478d5c8591bbf07
mizutombo <thomas.shultz52@gmail.com>
2016-09-27 17:42:10 -0700

    converted blogProjects data to JSON format ... worked on ajax-JSON code

--- scripts/projects.js
+++ scripts/projects.js
@@ -18,21 +18,6 @@ Project.prototype.toHtml = function() {
   return html;
 };
 
-/*Project.prototype.toHtml = function() {
-var $newProject = $('article.template').clone();
-$newProject.removeAttr('class');
-
-$newProject.attr('data-category', this.category);
-$newProject.find('a:first').text(this.developer);
-$newProject.find('a').attr('href', this.developerUrl);
-$newProject.find('h2').text(this.title); // return project title into <h2></h2> spot
-$newProject.find('h3').text(this.category); // return project category into <h3></h3> spot
-$newProject.find('time[pubdate]').attr('title', this.publishedOn);
-$newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-$newProject.find('h3.project-body').html(this.body); // return project description html into <h4></h4> spot
-
-return $newProject;
-};*/
 
 myLocalData.sort(function(currentElement, nextElement) {
   return (new Date(nextElement.publishedOn)) - (new Date(currentElement.publishedOn));
@@ -40,9 +25,12 @@ myLocalData.sort(function(currentElement, nextElement) {
 
 myLocalData.forEach(function(element) {
   projects.push(new Project(element));
+  console.log(element);
+  //push contents of projects into array
 });
 
 projects.forEach(function(project) {
   $('#project').append(project.toHtml());
+  console.log(project);
   //populate contents of projects via append onto section with id='project'
 });
