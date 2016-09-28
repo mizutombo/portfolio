d0d6a58b63bced03a4b370159af70419eb8db2bc
mizutombo <thomas.shultz52@gmail.com>
2016-09-27 21:07:32 -0700

    refactored and retrofitted code to enable projects template to render onto DOM

--- scripts/blogProjects.js
+++ scripts/blogProjects.js
@@ -1,7 +1,7 @@
-var ourLocalData = [
+var myLocalData = [
   {
     title:       'Portfolio',
-    category:    'jQuery, Javascript, HTML, CSS',
+    category:    'Code 301 personal project',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/portfolio/tree/class-02',
     publishedOn: '2016-09-20',
@@ -9,7 +9,7 @@ var ourLocalData = [
   },
   {
     title:       '4-the-Rodentz',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 team project',
     developer:   'Tom Shultz, Elizabeth Fillmore, Drew Stock, Jon Walz, Lee Zamastil',
     developerUrl:   'https://github.com/4-the-rodentz/App',
     publishedOn: '2016-08-18',
@@ -17,7 +17,7 @@ var ourLocalData = [
   },
   {
     title:       'Market Analysis',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 individual assignment',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/market-analysis-app/tree/class13',
     publishedOn: '2016-08-09',
@@ -25,7 +25,7 @@ var ourLocalData = [
   },
   {
     title:       'Cookie Stand',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 individual assignment',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/cookie-stand/tree/class09',
     publishedOn: '2016-07-26',
@@ -33,7 +33,7 @@ var ourLocalData = [
   },
   {
     title:       'Guessing Game',
-    category:    'Javascript, HTML, CSS',
+    category:    'Code 201 individual assignment',
     developer:   'Tom Shultz',
     developerUrl:   'https://github.com/mizutombo/guessing-game/tree/gh-pages',
     publishedOn: '2016-07-11',
