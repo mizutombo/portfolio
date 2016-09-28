f4b53dd1437280a4c1ab00c0184a214f355af177
mizutombo <thomas.shultz52@gmail.com>
2016-09-27 19:35:45 -0700

    removed extraneous muted code ... debugged and enabled proper functionality of Read on anchor link to display full text beyond teaser

--- scripts/projects.js
+++ scripts/projects.js
@@ -18,22 +18,6 @@ Project.prototype.toHtml = function() {
   return html;
 };
 
-/*Project.prototype.toHtml = function() {
-var $newProject = $('article.template').clone();
-$newProject.removeAttr('class');
-
-$newProject.attr('data-category', this.category);
-$newProject.find('a:first').text(this.developer);
-$newProject.find('a').attr('href', this.developerUrl);
-$newProject.find('h2').text(this.title); // return project title into <h2></h2> spot
-$newProject.find('h3').text(this.category); // return project category into <h3></h3> spot
-$newProject.find('time[pubdate]').attr('title', this.publishedOn);
-$newProject.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-$newProject.find('h3.project-body').html(this.body); // return project description html into <h4></h4> spot
-
-return $newProject;
-};*/
-
 myLocalData.sort(function(currentElement, nextElement) {
   return (new Date(nextElement.publishedOn)) - (new Date(currentElement.publishedOn));
 });
