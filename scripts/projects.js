e742b7e04fdcfc03fc041003c007c53d96fba86b
mizutombo <thomas.shultz52@gmail.com>
2016-09-29 17:19:50 -0700

    further progress in debugging AJAX-JSON code

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,23 +1,82 @@
-var projects = [];
+(function(module){
 
 function Project (opts) {
+  for (keys in opts) {
+    this[keys] = opts[keys];
+  }
+};
+
+/*var projects = [];*/
+
+Project.all = [];
+
+/*function Project (opts) {
   this.title = opts.title;
   this.category = opts.category;
   this.developer = opts.developer;
   this.developerUrl = opts.developerUrl;
   this.publishedOn = opts.publishedOn;
   this.body = opts.body;
-};
+};*/
 
 // Incorporate handlebars.js into code
 Project.prototype.toHtml = function() {
   var source = $('#project-template').html();
   var template = Handlebars.compile(source);
   var html = template(this);
-
   return html;
 };
 
+/*Article.prototype.toHtml = function(scriptTemplateId) {
+  var template = Handlebars.compile($(scriptTemplateId).text());
+  this.daysAgo = parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000);
+  this.publishStatus = this.publishedOn ? 'published ' + this.daysAgo + ' days ago' : '(draft)';
+  this.body = marked(this.body);
+  return template(this);
+};*/
+
+Project.loadAll = function(dataToPassIn) {
+  dataToPassIn.sort(function(currentElement, nextElement) {
+    return (new Date(nextElement.publishedOn)) - (new Date(currentElement.publishedOn));
+  }).forEach(function(element) {
+    Project.all.push(new Project(element));
+  });
+};
+
+Project.fetchAll = function() { /* retrieve data from local storage, if available */
+  if (localStorage.blogProjects) {
+    var localStoredData = JSON.parse(localStorage.getItem('blogProjects'));
+    console.log('data in local storage', localStoredData);
+    Project.loadAll(localStoredData);
+    projectView.renderIndexPage(); // retrieve & render data from blogProjects.json
+  } else {
+
+    $.ajax({ /* if data not in local storage, call Ajax */
+      url: '/data/blogProjects.json',
+      method: 'GET',
+      success: successHandler,
+      error: errorHandler
+    });
+  }
+    console.log('calling Ajax the man');
+};
+
+function successHandler(data) {
+  localStorage.setItem('/data/blogProjects.json', JSON.stringify(data));
+  Project.loadAll(data);
+  projectView.renderIndexPage(); // retrieve & render data from blogProjects.json
+  console.log('Data:', data);
+}
+function errorHandler(error) {
+  console.log('ERROR', error);
+}
+
+Project.fetchAll(); /* call function 'fetchAll'*/
+
+module.Project = Project;
+})(window);
+
+/*
 myLocalData.sort(function(currentElement, nextElement) {
   return (new Date(nextElement.publishedOn)) - (new Date(currentElement.publishedOn));
 });
@@ -30,3 +89,4 @@ projects.forEach(function(project) {
   $('#project').append(project.toHtml());
   //populate contents of projects via append onto section with id='project'
 });
+*/
--- scripts/projectsView.js
+++ scripts/projectsView.js
@@ -1,3 +1,5 @@
+(function(module){
+
 var projectView = {};
 
 projectView.populateFilters = function() {
@@ -6,16 +8,18 @@ projectView.populateFilters = function() {
     var category;
     var optionTag;
 
-    developer = $(this).find('div').attr('data-author'); // retrieves text
+    developer = $(this).find('div').attr('data-developer'); // retrieves text
     console.log(developer);
     optionTag = '<option value = "' + developer + '">' + developer + '</option>';
     if ($('#developer-filter option[value = "' + developer + '"]').length === 0) {
-      $('#developer-filter').append(optionTag); // populates Developer name for pull-down selector and appends via option tag, without duplicating Developer name in selector
+      $('#developer-filter').append(optionTag);
+      // populates Developer name for pull-down selector and appends via option tag, without duplicating Developer name in selector
     }
     category = $(this).find('div').attr('data-category'); // retrieves attribute and value
     optionTag = '<option value = "' + category + '">' + category + '</option>';
     if ($('#category-filter option[value = "' + category + '"]').length === 0) {
-      $('#category-filter').append(optionTag); // populates Project Category name for pull-down selector and appends via option tag, without duplicating Project Category name in selector
+      $('#category-filter').append(optionTag);
+      // populates Project Category name for pull-down selector and appends via option tag, without duplicating Project Category name in selector
     }
   });
 };
@@ -43,12 +47,15 @@ projectView.handleCategoryFilter = function() {
       $('article').hide(); // upon filter item change-click, hide all projects in 'article'
       var $same = $(this).val();
       $('article').each(function() {
-        if ($same === $(this).find('div').attr('data-category')) { // retrieves attribute and value
-          $(this).fadeIn(); // fade into 'article' view the click-selected item
+        if ($same === $(this).find('div').attr('data-category')) {
+          // retrieves attribute and value
+          $(this).fadeIn();
+          // fade into 'article' view the click-selected item
         }
       });
     } else {
-      $('article').show(); // if no filter item is selected, show all projects
+      $('article').show();
+      // if no filter item is selected, show all projects
     }
     $('#developer-filter').val('');
   });
@@ -57,11 +64,14 @@ projectView.handleCategoryFilter = function() {
 projectView.handleMainNav = function () {
   $('.main-nav').on('click', '.tab', function(event) {
     event.preventDefault();
-    $('.tab-content').hide(); // upon click of any navigation link, hide all '.tab-content' items ... i.e. hide all projects and contents of "About" page
+    $('.tab-content').hide();
+    // upon click of any navigation link, hide all '.tab-content' items ... i.e. hide all projects and contents of "About" page
     var $selectedItem = $(this).data('content');
-    $('#' + $selectedItem).fadeIn(); // fade in page linked to click-selected navigation link
+    $('#' + $selectedItem).fadeIn();
+    // fade in page linked to click-selected navigation link
   });
-  $('.main-nav .tab:first').click(); //calls jQuery event on navigation
+  $('.main-nav .tab:first').click();
+  //calls jQuery event on navigation
 };
 
 projectView.setTeasers = function() {
@@ -74,10 +84,25 @@ projectView.setTeasers = function() {
   });
 };
 
+projectView.renderIndexPage = function() {
+  console.log('render index page');
+  console.log('Project.all', Project.all);
+  Project.all.forEach(function(project){
+    $('#projects-post').append(project.toHtml());
+    if($('#category-filter option:contains("'+ project.category + '")').length === 0) {
+      $('#category-filter').append(project.toHtml());
+    }
+    if($('#developer-filter option:contains("'+ project.developer + '")').length === 0) {
+      $('#developer-filter').append(project.toHtml());
+    }
+  });
+};
+
 projectView.populateFilters();
 projectView.handleDeveloperFilter();
 projectView.handleCategoryFilter();
 projectView.handleMainNav();
 projectView.setTeasers();
 
-projectView.renderIndexPage(); // retrieve data from blogProjects.json
+module.projectView = projectView;
+})(window);
