d5c9cf94ae0df421d35a0b5a3b9ac44a06c2dd80
mizutombo <thomas.shultz52@gmail.com>
2016-10-06 06:41:31 -0700

    coded server.js for express server

--- server.js
+++ server.js
@@ -1,15 +1,27 @@
-// set up of basic Express static server
+var express = require('express'),
+requestProxy = require('express-request-proxy'),
+port = process.env.PORT || 3000,
+app = express();
 
-var express = require('express');
-var app = express();
+function proxyGitHub(request, response) {
+  console.log('routing GitHub request for', request.params[0]);
+  (requestProxy({
+    url: 'https://api.github.com/' + request.params[0],
+    headers: {
+      Authorization: 'token ' + process.env.GITHUB_TOKEN
+    }
+  }))(request, response);
+};
+
+app.get('/github/*', proxyGitHub);
 
 app.use(express.static('./'));
 
 app.get('*', function(request, response) {
-  console.log('NEW REQUEST FROM: ', request.url);
-  response.sendFile('index.html', {root: '.'});
+  console.log('new request:', request.url);
+  response.sendFile('index.html', { root: '.' });
 });
 
-app.listen(3000, function() {
-  console.log('SERVER IS UP ON PORT 3000');
+app.listen(port, function() {
+  console.log('server started on port ' + port + '!');
 });
